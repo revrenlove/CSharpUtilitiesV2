03e35b7ff3a02ebae33e3f22ca9014fd090e990b
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-07-14 12:26:27 -0500

    25 testsscripts (#26)
    * spell check
    
    * Updating gitignore
    
    * checkpoint
    
    * I think it works
    
    * TEsting works
    
    * eof
    
    * adding todo

---
+++ src/test/jim.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert';
+import { capitalizeFirstLetter } from '../utilities';
+import * as vscode from 'vscode';
+
+suite('Jim Test Suite', () => {
+    test('Jim test', () => {
+        // TODO: JE - Delete this file
+
+        const x = capitalizeFirstLetter("hi");
+        assert.strictEqual(x, "Hi");
+
+        const y = vscode.window.activeColorTheme;
+        vscode.window.showInformationMessage("SS");
+    });
+});
