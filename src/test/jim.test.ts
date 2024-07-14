5ff845344815fc9d50a1696ea8f96d7f99f9e3a1
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-07-13 20:06:53 -0500

    I think it works

--- src/test/jim.test.ts
+++ src/test/jim.test.ts
@@ -1,8 +1,17 @@
 import * as assert from 'assert';
+import { capitalizeFirstLetter } from '../utilities';
+import * as vscode from 'vscode';
 
 suite('Jim Test Suite', () => {
     test('Jim test', () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+        // throw new Error("AAAAA");
+        // assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        // assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+
+        const x = capitalizeFirstLetter("hi");
+        assert.strictEqual(x, "Hi");
+
+        const y = vscode.window.activeColorTheme;
+        vscode.window.showInformationMessage("SS");
     });
 });
