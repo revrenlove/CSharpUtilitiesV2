e7cd525c7e7adbb506403d9bb4928f663559c71a
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-07-15 19:06:37 -0500

    Adding base eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,19 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+    {
+        ignores: ["**/*.d.ts", "**/out", ".vscode-test", "**/*.mjs"],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
 );
