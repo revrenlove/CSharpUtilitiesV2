5e4522acc1e25fd143e2bb0bd2b6abeb7fae8d40
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-07-20 16:50:20 -0500

    Indentation lint works

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,36 @@
-// @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
     {
-        ignores: ["**/*.d.ts", "**/out", ".vscode-test", "**/*.mjs"],
+        ignores: ["**/*.d.ts", "**/out", ".vscode-test"],
     },
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
     {
+        plugins: {
+            // '@typescript-eslint': tseslint.plugin,
+            "@stylistic": stylistic,
+        },
         languageOptions: {
+            parser: tseslint.parser,
             parserOptions: {
                 project: true,
-                tsconfigRootDir: import.meta.dirname,
             },
         },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+        },
     },
-);
+    {
+        files: ["**/*.mjs"],
+        ...tseslint.configs.disableTypeChecked,
+    },
+    {
+        files: ["**/*.json"],
+        ...tseslint.configs.disableTypeChecked,
+    },
+)
--- src/extensionUserSettings.ts
+++ src/extensionUserSettings.ts
@@ -4,24 +4,24 @@ function getConfigurationValue<T>(section: string): T {
     
     const config = vscode.workspace.getConfiguration('c-sharp-utilities2');
 
-            const value = config.get(section) as T;
+    const value = config.get(section) as T;
     
-            return value;
+    return value;
 }
 
 function isFileScopedNamespace(): boolean {
-        const value = getConfigurationValue<boolean>('newItemTemplate.fileScopedNamespace');
-        return value;
-    }
+    const value = getConfigurationValue<boolean>('newItemTemplate.fileScopedNamespace');
+    return value;
+}
 
-    function isImplicitUsings(): boolean {
-        const value = !getConfigurationValue<boolean>('newItemTemplate.includeUsingStatements');
-        return value;
-    }
+function isImplicitUsings(): boolean {
+    const value = !getConfigurationValue<boolean>('newItemTemplate.includeUsingStatements');
+    return value;
+}
 
-    function namespacesToInclude(): string[] {
-        const value = getConfigurationValue<string[]>('newItemTemplate.namespacesToInclude');
-        return value;
-    }
+function namespacesToInclude(): string[] {
+    const value = getConfigurationValue<string[]>('newItemTemplate.namespacesToInclude');
+    return value;
+}
 
-    export {isFileScopedNamespace,isImplicitUsings,namespacesToInclude }
+export {isFileScopedNamespace,isImplicitUsings,namespacesToInclude }
